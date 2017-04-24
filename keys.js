81bfed76d47c3864ccc63602d48fce36f0152ef1
Shawn Assour <sassour@gmail.com>
2017-04-24 17:29:55 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'YnNjNHTkGK4OT3trcNTzSIx9H',
-  consumer_secret: 'C8Xqc6hd6WvW2euHXu9JJMdFOwrBkMCtb8bJ1yNIe7IOiVgFdt',
-  access_token_key: '1684011762-qT9MwGdcJ96KxnSgAhNxV23HL5mUjPDxgbumVk1',
-  access_token_secret: '3s237LF5hAFWyPJsuQPuBIXPEPg2Sit912OqZyVPMxO0L',
+  consumer_key: 'XXXXXXXXXXXXXXXX',
+  consumer_secret: 'XXXXXXXXXXXXXXXX',
+  access_token_key: 'XXXXXXXXXXXXXXXX',
+  access_token_secret: 'XXXXXXXXXXXXXXXX',
 }
